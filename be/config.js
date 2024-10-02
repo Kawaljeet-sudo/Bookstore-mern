4436a390e9042ff18ed944445bfa4b7791ea3e0f
kawal <kawaljeet.singh606@gmail.com>
2024-10-02 18:34:49 +0530

    be add

---
+++ be/config.js
@@ -0,0 +1,3 @@
+export const mongoDBURL = 'mongodb+srv://kawaljeetsingh606:7YcRdXLdwe1oIERG@books-store.cbjb0.mongodb.net/?retryWrites=true&w=majority&appName=books-store'
+
+export const baseURL = 'https://your-backend-api-url.onrender.com';
---
+++ be/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { mongoDBURL } from './config.js';
+import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
+import booksRoute  from './routes/bookRoutes.js';
+import cors from 'cors';
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+    return res.status(234).send('Welcome to mern')    
+})
+
+app.use('/books',booksRoute)
+
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log("app connected to database")
+    app.listen(PORT,() => {
+        console.log("app is listening to port 8080")
+    })
+}).catch(() => {
+    console.log(error);
+})
+
